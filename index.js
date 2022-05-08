5e51610d3530b7cfe2c45657132c54667199611e
Artt <fisol3640.art@gmail.com>
2022-05-09 01:25:53 +0700

    move function to a file

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 module.exports = {
-  keySelector: require('./lib/keySelector'),
-  keySelectorAll: require('./lib/keySelectorAll')
+  /**
+ * @name keySelector
+ * @description Find value for keys (using like query selector)
+ * @param {*} object
+ * @param {(string|string[])} keys
+ * @param {*} defaultValue
+ *
+ * @returns {*} resolved value or default value or null
+ */
+  keySelector: (input, keys, defaultValue = null) => require('./lib/key')(input, keys, defaultValue, false),
+  /**
+ * @name keySelectorAll
+ * @description Find value for keys (using like query selector)
+ * @param {*} object
+ * @param {(string|string[])} keys
+ * @param {*} defaultValue
+ *
+ * @returns {Array} resolved value or default value or []
+ */
+  keySelectorAll: (input, keys, defaultValue = []) => require('./lib/key')(input, keys, defaultValue, true)
 }
